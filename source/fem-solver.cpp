0a1e622b6c6efaa788d075c94154379290be26d7
Daniel Fernandes <danielfeenandes@gmail.com>
2024-08-28 21:47:23 -0300

    Compute Global K

--- source/fem-solver.cpp
+++ source/fem-solver.cpp
@@ -7,6 +7,8 @@
 using namespace emscripten;
 
 struct Element {
+    float start;
+    float end;
     float length;
     float stiffness;
     std::vector<float> R;
@@ -22,6 +24,8 @@ class FEMSolver {
             nodeInputDataUnknowns.resize(_nNodes, std::vector<float>(6.0, 0.0));
 
             elementsInputData.resize(_nElements, std::vector<float>(4.0, 0.0));
+
+            K.resize(_nNodes*2, std::vector<float>(_nNodes*2, 0.0));
         }
 
         void SetSizes(int _nNodes, int _nElements){
@@ -65,28 +69,34 @@ class FEMSolver {
             for(int i = 0; i < nElements; i++){
                 ComputeElementProperties(elementsInputData[i]);
             };
+
+            for(int i = 0; i < elements.size(); i++){
+                ComputeGlobalK(elements[i]);
+            };
+
+            printf("Display K:\n");
+            for (int i = 0; i < nNodes*2; i++) {
+                for (int j = 0; j < nNodes*2; j++) {
+                    printf("%i,%i: %f\n", i,j,K[i][j]);
+                }
+            }
+
         }
 
         void ComputeElementProperties(std::vector<float> elementInput){
-            printf("Computing: \n");
             int start = elementInput[0];
             int end = elementInput[1];
-            printf("Start: %i, End: %i \n", start, end);
 
             float x_coord_start = nodeInputData[start][0];
             float x_coord_end = nodeInputData[end][0];
-            printf("X Start: %f, X End: %f \n", x_coord_start, x_coord_end);
 
             float y_coord_start = nodeInputData[start][1];
             float y_coord_end = nodeInputData[end][1];
-            printf("Y Start: %f, Y End: %f \n", y_coord_start, y_coord_end);
 
             float deltaX = x_coord_end - x_coord_start;
             float deltaY = y_coord_end - y_coord_start;
-            printf("deltaX: %f, deltaY: %f \n", deltaX, deltaY);
 
             float length = sqrt(pow(deltaX, 2) + pow(deltaY, 2));
-            printf("2: %f, 3: %f \n", elementInput[2],  elementInput[3]);
             float stiffness = elementInput[3] * elementInput[2] / length;
 
             float c = deltaX / length;
@@ -98,13 +108,57 @@ class FEMSolver {
             newEl.length = length;
             newEl.stiffness = stiffness;
             newEl.R = R;
-
-            printf("New Element: \n");
-            printf("Length: %f \n", newEl.length);
-            printf("Stiffness: %f \n", newEl.stiffness);
-            printf("R: %zu\n", newEl.R.size());
-           
+            newEl.start = start;
+            newEl.end = end;
+            
             elements.push_back(newEl);
+        }
+
+        void ComputeGlobalK(Element element){
+            std::vector<std::vector<float>> tempK;
+            tempK.resize(nNodes*2, std::vector<float>(nNodes*2, 0.0));
+
+            int indicesListSize = nNodes*2;
+            std::vector<int> indicesList;
+            for (int i = 0; i < indicesListSize; i++) {
+                indicesList.push_back(i);
+            }
+
+            std::vector<std::vector<int>> indices2D;
+            for (int i = 0; i < indicesListSize; i+=2) {
+                indices2D.push_back(std::vector<int>{indicesList[i], indicesList[i+1]});
+            }
+
+            std::vector<int> indicesFlat;
+            for (int i = 0; i < indices2D.size(); i++) {
+
+                if(i == element.start){
+                    indicesFlat.push_back(indices2D[i][0]);
+                    indicesFlat.push_back(indices2D[i][1]);
+                }
+            }
+
+            for (int i = 0; i < indices2D.size(); i++) {
+                if (i == element.end){
+                    indicesFlat.push_back(indices2D[i][0]);
+                    indicesFlat.push_back(indices2D[i][1]);
+                }
+            }
+
+
+            for (int i = 0; i < indicesFlat.size(); i++) {
+                for (int j = 0; j < indicesFlat.size(); j++) {
+                    int x = indicesFlat[i];
+                    int y = indicesFlat[j];
+                    tempK[x][y] = element.R[(i * 4) + j] * element.stiffness;
+                }
+            }
+            
+            for (int i = 0; i < nNodes*2; i++) {
+                for (int j = 0; j < nNodes*2; j++) {
+                    K[i][j] += tempK[i][j];
+                }
+            }
         }
 
         void Solve(){
@@ -117,7 +171,7 @@ class FEMSolver {
         std::vector<std::vector<float>> nodeInputData;
         std::vector<std::vector<float>> nodeInputDataUnknowns;
         std::vector<std::vector<float>> elementsInputData;
-
+        std::vector<std::vector<float>> K;
         std::vector<Element> elements;
 };
 
@@ -129,6 +183,6 @@ EMSCRIPTEN_BINDINGS(FemSolver) {
     .function("SetNodeInputData", &FEMSolver::SetNodeInputData)
     .function("SetElementInputData", &FEMSolver::SetElementInputData)
 
-    .function("Compute", &FEMSolver::Compute)
+    .function("ProcessData", &FEMSolver::ProcessData)
     ;
 }
